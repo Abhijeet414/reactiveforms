f650d335ba971b771ea66b29e7e5c80126ea78e1
ABHIJEET MANDAL <abhijeet.mandal@serialxtech.com>
2021-06-12 10:52:09 +0530

    reactiveform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  msg: string = "";
+  myform: FormGroup;
+  constructor() {
+    this.myform = new FormGroup({
+      firstname: new FormControl("Wills", [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern("^[a-zA-Z ]*$")]),
+      lastname: new FormControl("", [Validators.required, Validators.minLength(5), Validators
+        .maxLength(20), Validators.pattern("^[a-zA-Z ]*$")]),
+
+      email: new FormControl("", [Validators.required, Validators.pattern("^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$")]),
+
+      phone: new FormControl("", [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+      password: new FormControl("", [Validators.required,Validators.minLength(6),Validators.maxLength(6)]),
+      repassword: new FormControl("", [Validators.required,Validators.minLength(6),Validators.maxLength(6)]),
+
+  }
+  );
+}
+onRegisterClick() {
+  if (this.myform.valid) {
+    this.msg = "First Name: " + this.myform.controls.firstname.value +
+      "<br>Last Name: " + this.myform.controls.lastname.value +
+      "<br>Email: " + this.myform.controls.email.value +
+      "<br>Phone: " + this.myform.controls.phone.value +
+      "<br>Password: " + this.myform.controls.password.value +
+      "<br>Repassword: " + this.myform.controls.repassword.value;
+  }
+  else {
+    this.msg = "Invalid";
+  }
+}
 }
